b643a3b32f6653700d3ddf8e43d13aa085ac1564
Tom Atkins <tom.atkins@algolia.com>
2023-04-03 19:13:58 +0100

    Add basic error handling for non existing questions

--- src/routes/question/[questionId]/+page.server.ts
+++ src/routes/question/[questionId]/+page.server.ts
@@ -1,10 +1,19 @@
 import { getQuestionById } from '$lib/server/db';
+import { error, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (({params}) => {
     const questionId = parseInt(params.questionId);
 
+    if (!questionId) {
+        throw error(404, "Question not found");
+    }
+
     const question = getQuestionById(questionId);
+
+    if (!question) {
+        throw error(404, "Question not found");
+    }
 
     return {
         question
